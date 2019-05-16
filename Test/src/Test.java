e196e35a2187f947be8eede6d6f893d8ddf0dcb1
leeseungmok <48539283+leeseungmok@users.noreply.github.com>
2019-05-16 10:55:48 +0900

    Add files via upload

---
+++ Test/src/Test.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void amain(int i, int j){
+		int a = (i+j);
+	}
+	
+	public static void bain(int i, int j){
+		int b = (i-j);
+	}
+	
+	public static void cmain(int i, int j){
+		int c = (i*j);
+	}
+	
+	public static void dmain(int i, int j){
+		int d = (i/j);
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int ii = scan.nextInt();
+		int jj = scan.nextInt();
+        String st = scan.next();
+        
+        System.out.print("정수입력:");
+        
+		switch (st) {
+		case "+":
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+		switch (st) {
+		case "-":
+			
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+		switch (st) {
+		case "*":
+			
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+		switch (st) {
+		case "/":
+			
+			
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
