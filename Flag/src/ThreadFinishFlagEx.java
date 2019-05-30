45f4245e6ea58aeb65cd08e395e52f1214d6c7c6
leeseungmok <48539283+leeseungmok@users.noreply.github.com>
2019-05-30 14:02:57 +0900

    Add files via upload

---
+++ Flag/src/ThreadFinishFlagEx.java
@@ -0,0 +1,33 @@
+import java.awt.Container;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+
+public class ThreadFinishFlagEx extends JFrame{
+	private RandomThread th;
+	
+	public ThreadFinishFlagEx(){
+		setTitle("ThreadFinishFlagEx 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(null);
+		
+		c.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e){
+				th.finish();
+			}
+		});
+		setSize(300,200);
+		setVisible(true);
+		
+		th = new RandomThread(c); // 스레드 생성
+		th.start();
+	}
+
+	public static void main(String[] args) {
+		new ThreadFinishFlagEx();
+
+	}
+
+}
